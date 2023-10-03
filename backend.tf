5d37a97fdaea0aece0144d7b4287fad1f03dc34e
Youssef Afify <joe.afify95@gmail.com>
2023-10-04 00:44:16 +0300

    backend off

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-bucket-505"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    # encrypt        = true
-    dynamodb_table = "terraform-tfstate-locking"
-    profile        = "terraform"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-bucket-505"
+#     key            = "terraform.tfstate"
+#     region         = "us-east-1"
+#     # encrypt        = true
+#     dynamodb_table = "terraform-tfstate-locking"
+#     profile        = "terraform"
+#   }
+# }
